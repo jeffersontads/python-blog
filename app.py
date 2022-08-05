9912c9d33dda92012de3b584cd2a461b3c989089
jeffersonsotto <jeffersontads@hotmail.com>
2022-08-05 16:57:12 -0300

    feature: create projects and db conn

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from pydoc import describe
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import null 
+from sqlalchemy import null
 
 app = Flask(__name__)
 app.secret_key = "30302220jefferson"
 
-userpass = "30302220"
+userpass = "mysql+pymysql://root:@"
 basedir = "127.0.0.1"
 dbname = "/companydb"
 
@@ -15,25 +15,27 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+
 class Noticias(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     titulo = db.Column(db.String(100), nullable=False)
     descricao = db.Column(db.String(1000), nullable=False)
     categoria = db.Column(db.String(100), nullable=False)
     data = db.Column(db.String(6), nullable=False)
-    
+
     def __init__(self, titulo, descricao, categoria, data):
         self.titulo = titulo
         self.descricao = descricao
         self.categoria = categoria
         self.data = data
-    
-    
+
+
 @app.route('/')
-def index():    
+def index():
+    noticias = db.session.query(Noticias)
     return render_template('index.html')
 
 
 @app.route('/input')
-def input_data():    
+def input_data():
     return render_template('input.html')
