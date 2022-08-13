0b81aaa670160a4f7a244e5a20e31205f68fa01e
jeffersonsotto <jeffersontads@hotmail.com>
2022-08-13 20:45:49 -0300

    feature: implemets method login

--- src/app.py
+++ src/app.py
@@ -3,9 +3,14 @@ from distutils import config
 from config import config
 from flask import Flask, render_template, redirect, url_for, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
+
+from flask_login import UserMixin, login_user, logout_user
 
 
 # instancia
@@ -24,6 +29,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
 
 db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
 
 
 class Noticias(db.Model):
@@ -46,7 +53,7 @@ class Noticias(db.Model):
         self.image_data = image_data
 
 
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(102), nullable=False)
@@ -54,8 +61,11 @@ class Users(db.Model):
 
     def __init__(self, username, password, fullname):
         self.username = username
-        self.password = password
+        self.password = generate_password_hash(password)
         self.fullname = fullname
+
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
 
 
 @ app.route('/')
@@ -68,25 +78,33 @@ def index():
 
 # TODO: TERMINAR A LOGICA DE LOGAR NO SISTEMA, REGISTRAR NOVOS USUARIOS E CRIAR PAINEL ADMIN
 
+@login_manager.user_loader
+def get_user(id):
+    return Users.query.filter_by(id=id).first()
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    data_users = db.session.query(Users)
+    #data_users = db.session.query(Users)
     if request.method == 'POST':
-        # print(request.form['username'])
-        # print(request.form['password'])
-        userdetails = request.form
-        username = userdetails['username']
-        password = userdetails['password']
+        username = request.form['username']
+        password = request.form['password']
+        user = Users.query.filter(username == username).first()
 
-        if password == 'jefferson':
-            return render_template('index.html', data=data_users)
+        if not user.verify_password(password):
+            return redirect(url_for('login'))
+
         else:
-            return "SUCCESS!,Successfully entered into the Database"
-    else:
-        return render_template('auth/login.html')
+            return redirect(url_for('index'))
 
 
-@ app.route('/input', methods=['GET', 'POST'])
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('auth/login.html'))
+
+
+@app.route('/input', methods=['GET', 'POST'])
 def input_data():
     if request.method == 'POST':
         titulo = request.form['titulo']
@@ -144,6 +162,20 @@ def delete(id):
     flash('Notícia deletada com sucesso!')
 
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        user = Users(username, password, email)
+        db.session.add(user)
+        db.session.commit()
+
+    return render_template('register.html')
 
 
 if __name__ == "__main__":
